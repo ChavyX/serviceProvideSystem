dc91e5f101d56613e5078a95b8111b219e0f2944
ChavyX <1175450568@qq.com>
2021-06-05 14:33:33 +0800

    add

---
+++ src/main/java/com/chavy/www/service_provide/domain/IContractDomain.java
@@ -0,0 +1,11 @@
+package com.chavy.www.service_provide.domain;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.chavy.www.service_provide.module.Contract;
+
+public interface IContractDomain {
+
+    boolean addContract(Contract contract);
+
+    Page<Contract> selectContract(Contract contract, int size, int current);
+}
---
+++ src/main/java/com/chavy/www/service_provide/module/Contract.java
@@ -0,0 +1,112 @@
+package com.chavy.www.service_provide.module;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.util.Date;
+
+@TableName(value = "contract")
+public class Contract {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @TableField(value = "project_id")
+    private Integer projectId;
+
+    @TableField(value = "user_id")
+    private Integer userId;
+
+    @TableField(value = "content")
+    private String content;
+
+    @TableField(value = "start_time")
+    private Date startTime;
+
+    @TableField(value = "valid_time")
+    private Date validTime;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return project_id
+     */
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    /**
+     * @param projectId
+     */
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    /**
+     * @return user_id
+     */
+    public Integer getUserId() {
+        return userId;
+    }
+
+    /**
+     * @param userId
+     */
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * @return content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return start_time
+     */
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    /**
+     * @param startTime
+     */
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    /**
+     * @return valid_time
+     */
+    public Date getValidTime() {
+        return validTime;
+    }
+
+    /**
+     * @param validTime
+     */
+    public void setValidTime(Date validTime) {
+        this.validTime = validTime;
+    }
+}
