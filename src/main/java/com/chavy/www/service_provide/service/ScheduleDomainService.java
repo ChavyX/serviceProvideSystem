f780d1426ff1ee6542373b44954eeaa0ce0f6c02
ChavyX <1175450568@qq.com>
2021-06-05 14:25:50 +0800

    add

---
+++ src/main/java/com/chavy/www/service_provide/service/ScheduleDomainService.java
@@ -0,0 +1,15 @@
+package com.chavy.www.service_provide.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.chavy.www.service_provide.module.Schedule;
+
+public interface ScheduleDomainService {
+
+    boolean addSchedule(Schedule schedule);
+
+    boolean deleteSchedule(Integer id);
+
+    boolean updateSchedule(Schedule schedule);
+
+    Page<Schedule> selectSchedule(Integer schedule, int size ,int current);
+}
