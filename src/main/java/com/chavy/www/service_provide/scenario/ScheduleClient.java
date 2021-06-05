6f4bb1094132dda722163446ddce6ce993770c1e
ChavyX <1175450568@qq.com>
2021-06-05 15:03:57 +0800

    add

---
+++ src/main/java/com/chavy/www/service_provide/scenario/ScheduleClient.java
@@ -0,0 +1,38 @@
+package com.chavy.www.service_provide.scenario;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.chavy.www.service_provide.module.Schedule;
+import com.chavy.www.service_provide.result.CommonResult;
+import com.chavy.www.service_provide.service.ScheduleDomainService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController("/schedule")
+public class ScheduleClient {
+
+    @Resource
+    private ScheduleDomainService scheduleDomainService;
+
+    @PostMapping("/add")
+    public CommonResult<Void> add(@RequestBody Schedule schedule) {
+        return CommonResult.of(scheduleDomainService.addSchedule(schedule));
+    }
+
+    @PostMapping("/update")
+    public CommonResult<Void> update(@RequestBody Schedule schedule) {
+        return CommonResult.of(scheduleDomainService.updateSchedule(schedule));
+    }
+
+    @GetMapping("/delete")
+    public CommonResult<Void> delete(@RequestParam("id") Integer id) {
+        return CommonResult.of(scheduleDomainService.deleteSchedule(id));
+    }
+
+    @GetMapping("select")
+    public CommonResult<Page<Schedule>> select(@RequestParam("id") Integer id,
+                                               @RequestParam(value = "size", defaultValue = "10") int size,
+                                               @RequestParam(value = "current", defaultValue = "1") int current) {
+        return new CommonResult<Page<Schedule>>().success(scheduleDomainService.selectSchedule(id, size, current));
+    }
+}
